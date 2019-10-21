8ea7c3466df387e4d91fb1e34d3cf746e753c319
R. Miles McCain <x1-key@sendmiles.email>
2019-10-21 16:45:18 -0400

    Add generalizable backend

--- openalerts/management/backends.py
+++ openalerts/management/backends.py
@@ -3,14 +3,18 @@ Backend to support OIDC login
 """
 
 # Django
-from django.conf import settings
+import os
 
 # Third Party
 from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 class PressPassBackend(OpenIdConnectAuth):
-    """Authentication Backend for PressPass OpenId"""
+    """Authentication backend for PressPass OpenId"""
     name = 'presspass'
-    OIDC_ENDPOINT = settings.PRESSPASS_URL + '/openid'
-    CLIENT_ID = "147292"
+    OIDC_ENDPOINT = os.getenv("PRESSPASS_BACKEND_URL", 'https://passpass.it/openid')
     DEFAULT_SCOPE = ['openid', 'profile', 'email', 'organizations']
+
+    def get_user_details(self, response):
+        details = super().get_user_details(response)
+        details["organizations"] = response["organizations"]
+        return details
