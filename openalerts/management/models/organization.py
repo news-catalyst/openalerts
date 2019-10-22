7d1f0582826350c602b585bd99c654d92a93cbe2
R. Miles McCain <x1-key@sendmiles.email>
2019-10-22 14:28:01 -0400

    Improve & initialize org model

--- openalerts/management/models/organization.py
+++ openalerts/management/models/organization.py
@@ -4,4 +4,17 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=128)
     presspass_uuid = models.UUIDField()
-    website = models.URLField()
+    website = models.URLField(blank=True, default='')
+
+    @staticmethod
+    def for_presspass_org(org):
+        if not Organization.objects.filter(presspass_uuid=org["uuid"]).exists():
+            return Organization.objects.create(name=org["name"], presspass_uuid=org["uuid"])
+        return Organization.objects.get(presspass_uuid=org["uuid"])
+
+    @staticmethod
+    def for_session(session):
+        orgs = []
+        for org in session["organizations"]:
+            orgs.append(Organization.for_presspass_org(org))
+        return orgs
