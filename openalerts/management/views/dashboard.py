5c7bd7b03ea0ee80e5bc4e5e81492d4793113c20
R. Miles McCain <x1-key@sendmiles.email>
2019-10-22 14:29:32 -0400

    Create sessions for dashboard view

--- openalerts/management/views/dashboard.py
+++ openalerts/management/views/dashboard.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
 from management.mixins import SessionAuthenticationRequiredMixin
 
+from management.models import Organization
 
 class DashboardView(SessionAuthenticationRequiredMixin, TemplateView):
     template_name = "management/pages/dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["organizations"] = self.request.session["organizations"]
+        context["organizations"] = Organization.for_session(self.request.session)
         return context
