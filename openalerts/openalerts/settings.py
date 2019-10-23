1b129c3a0db17887344117c9d14fcac9ba215740
R. Miles McCain <x1-key@sendmiles.email>
2019-10-23 16:41:20 -0400

    Standardize & improve form styling

--- openalerts/openalerts/settings.py
+++ openalerts/openalerts/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'bulma',
     'social_django',
     'alerts',
     'management',
