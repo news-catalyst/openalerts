d34b4e6cfb078e8a1b2fb1ee50f47456c8f4c22c
R. Miles McCain <x1-key@sendmiles.email>
2019-10-29 11:59:43 -0400

    Add real colorpicker

--- openalerts/management/views/organization.py
+++ openalerts/management/views/organization.py
@@ -1,6 +1,7 @@
 from django.views.generic.edit import UpdateView
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404, reverse
+from django import forms
 from management.mixins import SessionAuthenticationRequiredMixin, SessionOrgContextMixin, OrgContextMixin
 from management.models import Organization
 
@@ -12,6 +13,11 @@ class EditOrganizationView(
     model = Organization
     fields = ["name", "website", "description", "logo_url", "primary_color"]
     pk_url_kwarg = "org_id"
+
+    def get_form(self, *args, **kwargs):
+        form = super(EditOrganizationView, self).get_form(*args, **kwargs)
+        form["primary_color"].field.widget.input_type = "color"
+        return form
     
     def get_object(self):
         return get_object_or_404(Organization, id=self.kwargs["org_id"])
