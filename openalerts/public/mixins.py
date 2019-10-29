a0fb6871c870423491b2da88c31f8865e06002fe
R. Miles McCain <x1-key@sendmiles.email>
2019-10-29 12:50:33 -0400

    Add dynamic hidden org storage

--- openalerts/public/mixins.py
+++ openalerts/public/mixins.py
@@ -5,7 +5,16 @@ from django.shortcuts import get_object_or_404
 class OrganizationMixin(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super(OrganizationMixin, self).get_context_data(**kwargs)
-        if "org" in kwargs:
-            context["organization"] = get_object_or_404(Organization, id=kwargs["org"])
+
+        # Fallback to session-stored organization
+        if "org" not in self.request.GET and "public_org" in self.request.session:
+            context["organization"] = get_object_or_404(Organization, id=self.request.session.get("public_org"))
+            return context
+
+        if "org" in self.request.GET:
+            context["organization"] = get_object_or_404(Organization, id=self.request.GET.get("org"))
+            self.request.session["public_org"] = context["organization"].id
         else:
-            context["organzation"] = get_object_or_404(Organization, hostname=self.request.get_host())
+            context["organzation"] = get_object_or_404(Organization, custom_hostname=self.request.get_host())
+
+        return context
