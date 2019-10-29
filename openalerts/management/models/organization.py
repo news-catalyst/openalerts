5c49cb28d788368e4cd3eb55f446deb44b5f4650
R. Miles McCain <x1-key@sendmiles.email>
2019-10-29 11:50:53 -0400

    Fix org edit view

--- openalerts/management/models/organization.py
+++ openalerts/management/models/organization.py
@@ -7,8 +7,8 @@ class Organization(models.Model):
     presspass_uuid = models.UUIDField(editable=False)
     website = models.URLField(blank=True, default='')
     description = models.TextField(blank=True, default='')
-    primary_color = models.CharField(max_length=7, default="#444444")
-    logo_url = models.URLField(null=True, blank=True)
+    primary_color = models.CharField(max_length=7, default="#444444", help_text="Your news organization's primary brand color.")
+    logo_url = models.URLField(null=True, blank=True, help_text="The URL of your news organization's logo (for use on public facing pages).")
 
     @staticmethod
     def for_presspass_org(org):
--- openalerts/management/views/organization.py
+++ openalerts/management/views/organization.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import UpdateView
 from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404, reverse
 from management.mixins import SessionAuthenticationRequiredMixin, SessionOrgContextMixin, OrgContextMixin
 from management.models import Organization
 
@@ -9,8 +10,15 @@ class EditOrganizationView(
 ):
     template_name = "management/pages/edit_organization.html"
     model = Organization
-    fields = ["name", "website", "description"]
+    fields = ["name", "website", "description", "logo_url", "primary_color"]
     pk_url_kwarg = "org_id"
+    
+    def get_object(self):
+        return get_object_or_404(Organization, id=self.kwargs["org_id"])
+
+    def get_success_url(self):
+        print(self.object.id)
+        return reverse("management:organization", args=[self.object.id])
 
 class OrganizationView(
     SessionAuthenticationRequiredMixin, SessionOrgContextMixin, DetailView
