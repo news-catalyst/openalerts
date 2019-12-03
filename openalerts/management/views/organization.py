97233b629ef9e18e13cf504d574a1647d8b711c3
R. Miles McCain <x1-key@sendmiles.email>
2019-12-03 14:25:52 -0500

    Add more charts

--- openalerts/management/views/organization.py
+++ openalerts/management/views/organization.py
@@ -1,6 +1,8 @@
 from django.views.generic.edit import UpdateView
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404, reverse
+from django.utils import timezone
+from datetime import timedelta, datetime
 from django import forms
 from management.mixins import SessionAuthenticationRequiredMixin, SessionOrgContextMixin, OrgContextMixin
 from management.models import Organization
@@ -36,9 +38,29 @@ class OrganizationView(
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
+
+        email_subscribers = EmailSubscription.objects.filter(organization=self.object)
+        webpush_subscribers = WebpushSubscription.objects.filter(organization=self.object)
+
         data["subscribers"] = {
             "keys": str(["Email", "Web Alerts"]),
-            "values": str([EmailSubscription.objects.filter(organization=self.object).count(),
-                           WebpushSubscription.objects.filter(organization=self.object).count()])
+            "values": str([email_subscribers.count(),
+                           webpush_subscribers.count()]),
+            "count": email_subscribers.count() + webpush_subscribers.count(),
         }
+
+        subscribers = list(email_subscribers) + list(webpush_subscribers)
+        times = reversed([timezone.now() - timedelta(n) for n in range(90)]) # last 90 days
+
+        data["subscribers_timeseries"] = {
+            "label": "Currently Active Subscribers",
+            "values": str([{
+                "t": time.isoformat(),
+                "y": len([subscription for subscription in subscribers if subscription.created <= time])
+                # Deleted subscribers will not be shown in the chart, hence "current"
+            } for time in times])
+        }
+
+        print(data["subscribers_timeseries"])
+
         return data
